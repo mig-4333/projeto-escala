9aa8d4ae7babaaf500602fdd6f8e7f18b3b238f3
Miguel Guerra <rochamiguelguerra@gmail.com>
2026-03-07 17:12:06 -0300

    feature(disponibilidade): criando os controllers para a funcionalidade de registro de disponibilidade

--- src/controllers/disponibilidadeControllers.js
+++ src/controllers/disponibilidadeControllers.js
@@ -5,6 +5,20 @@ import Disponibilidade from "../models/disponibilidadeModels";
 export const disponibilidadeGET = async (req,res) => {
     const missas = await Missa.buscaMissas(req.session.pastoral_id);
     const liderancas = await Lideranca.buscaLiderancas(req.session.pastoral_id);
-            console.log(liderancas)
     res.render("disponibilidade", { missas, liderancas });
+};
+
+export const buscaDadosModalDisponibilidade = async (req, res) => {
+    const id_missa = req.params.id;
+    const disponibilidades =  await Disponibilidade.buscaDisponibilidadesMissaEspecifica(id_missa);
+    res.json( {disponibilidades} );
+};
+
+export const registra_disponibilidade = async (req,res) => {
+    const id_missa = req.body.id_missa;
+        console.log(id_missa)
+    delete req.body.id_missa;
+    const dados_disponibilidade = req.body;
+    await Disponibilidade.registraDisponibilidades(dados_disponibilidade, id_missa);
+    res.redirect("/disponibilidade");
 };
